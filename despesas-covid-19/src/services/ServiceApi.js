7de7c58dd79b183de8a2bb4e3fcbd3685c34f5b8
diego araujo <diego.santana@iteris.com.br>
2021-02-03 16:58:37 -0300

    atualização retorno valor

--- despesas-covid-19/src/services/ServiceApi.js
+++ despesas-covid-19/src/services/ServiceApi.js
@@ -1,9 +1,12 @@
-import a from './requisicao.json';
+import profile from './requisicao.json';
+
+export let dataCOVID19 = [];
 
 export const getCovidSpendingByMonthYear = async () => {
   return new Promise((resolve) =>
     setTimeout(() => {
-      resolve(a.Profile);
+      dataCOVID19 = profile.Profile;
+      resolve(profile.Profile);
     }, 2000)
   );
 };
