f76e95d64481bd3ebe19647672df533b82eaa726
Thiago Dipold Cavalcanti - TCV <thiago.cavalcanti@iteris.com.br>
2021-02-05 11:49:50 -0300

    correção na condição de despesas no component de mensagem boas vindas

--- despesas-covid-19/src/organisms/BoxMensagemBoasVindas.jsx
+++ despesas-covid-19/src/organisms/BoxMensagemBoasVindas.jsx
@@ -5,7 +5,7 @@ import { DespesasContext } from '../providers/despesas-context';
 const BoxMensagemBoasVindas = () => {
   const { despesas } = React.useContext(DespesasContext);
 
-  if (despesas.length === 0) {
+  if (despesas && despesas.length === 0) {
     return <MensagemBoasVindasContainer></MensagemBoasVindasContainer>;
   } else {
     return <></>;
