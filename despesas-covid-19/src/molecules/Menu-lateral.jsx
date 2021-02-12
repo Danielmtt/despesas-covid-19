7daf0340ca9ded55275c7a6e90abcc099e48b59a
Thiago Dipold Cavalcanti - TCV <thiago.cavalcanti@iteris.com.br>
2021-02-12 17:28:02 -0300

    linha-ano com typescript

--- despesas-covid-19/src/molecules/Menu-lateral.jsx
+++ despesas-covid-19/src/molecules/Menu-lateral.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import LinhaAno from '../atoms/Linha-ano.jsx';
+import LinhaAno from '../atoms/Linha-ano.tsx';
 import TreeView from '@material-ui/lab/TreeView';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import ChevronRightIcon from '@material-ui/icons/ChevronRight';
