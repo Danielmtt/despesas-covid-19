ed6b4bb7384e450a8d812a5833097cb5eb7a0e6d
Thiago Dipold Cavalcanti - TCV <thiago.cavalcanti@iteris.com.br>
2021-02-15 10:51:54 -0300

    inserção do roteamento na aplicação

--- despesas-covid-19/src/pages/App.js
+++ despesas-covid-19/src/pages/App.js
@@ -1,14 +1,53 @@
 import './App.css';
-import DespesasCovid from '../templates/Despesas-covid';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  NavLink,
+} from 'react-router-dom';
+import DespesasCovidPage from './DespesasCovidPage';
+import DespesasBolsaFamiliaPage from './DespesasBolsaFamiliaPage';
 import React from 'react';
-import { ToastContainer } from 'react-toastify';
 
 function App() {
   return (
-    <>
-      <ToastContainer />
-      <DespesasCovid></DespesasCovid>
-    </>
+    <Router>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/" exact>
+              Home
+            </NavLink>
+          </li>
+          <li>
+            <NavLink to="/despesas/covid-19">Despesas Covid-19</NavLink>
+          </li>
+          <li>
+            <NavLink to="/despesas/bolsa-familia">
+              Despesas Bolsa Família
+            </NavLink>
+          </li>
+        </ul>
+      </nav>
+
+      <header>
+        <Switch>
+          <Route path="/despesas/covid-19">
+            <DespesasCovidPage />
+          </Route>
+
+          <Route path="/despesas/bolsa-familia">
+            <DespesasBolsaFamiliaPage />
+          </Route>
+
+          <Route path="/">
+            <div>
+              <h1>Homepage de despesas</h1>
+            </div>
+          </Route>
+        </Switch>
+      </header>
+    </Router>
   );
 }
 
---
+++ despesas-covid-19/src/pages/DespesasCovidPage.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import DespesasCovid from '../templates/Despesas-covid';
+import { ToastContainer } from 'react-toastify';
+
+const DespesasCovidPage = () => {
+  return (
+    <>
+      <ToastContainer />
+      <DespesasCovid></DespesasCovid>
+    </>
+  );
+}
+
+export default DespesasCovidPage
