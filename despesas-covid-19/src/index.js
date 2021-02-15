49f4e459e1d5450699f1a48500c0e7bb5f9a0824
diagom <d-asantana@live.com>
2021-02-15 12:34:52 -0300

    Adicionar router

--- despesas-covid-19/src/index.js
+++ despesas-covid-19/src/index.js
@@ -1,13 +1,46 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import './index.css';
+// import Home from './pages/Home.tsx';
+// import reportWebVitals from './pages/reportWebVitals';
+// import { ToastContainer } from 'react-toastify';
+// import App from './pages/App';
+// import { DespesasProvider } from './providers/despesas-context';
+
+// ReactDOM.render(
+//   <>
+//     <ToastContainer />
+//     <Home />
+//   </>,
+//   document.getElementById('root')
+// );
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import DespesasCovidPage from './pages/DespesasCovidPage.tsx';
+import DespesasBolsaFamiliaPage from './pages/DespesasBolsaFamiliaPage.tsx';
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './pages/Home.tsx';
-import { ToastContainer } from 'react-toastify';
+import { DespesasProvider } from './providers/despesas-context';
 
 ReactDOM.render(
-  <>
-    <ToastContainer />
-    <Home />
-  </>,
+  <DespesasProvider>
+    <Router>
+      <Switch>
+        <Route path="/despesas/covid-19">
+          <DespesasCovidPage />
+        </Route>
+
+        <Route path="/despesas/bolsa-familia">
+          <DespesasBolsaFamiliaPage />
+        </Route>
+
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  </DespesasProvider>,
   document.getElementById('root')
 );
