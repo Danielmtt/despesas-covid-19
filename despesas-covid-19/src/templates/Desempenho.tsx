a03adbb8273043770b4211d1a1fb869aaf84495e
Maria Gabriela Franco <maria.franco@iteris.com>
2021-02-24 11:29:07 -0300

    adição do título e menu superior

--- despesas-covid-19/src/templates/Desempenho.tsx
+++ despesas-covid-19/src/templates/Desempenho.tsx
@@ -2,10 +2,12 @@ import React from 'react';
 import Teste from '../atoms/atoms-desempenho/Atom-teste';
 import OrganismeTabPanel from '../organisms/organisms-desempenho/Organisme-Tab-Panel';
 import AtomFormularioColaborador from '../organisms/organisms-desempenho/Atom-Fomulario-Colaborador';
+import BoxPageTitleDesempenho from '../organisms/organisms-desempenho/BoxPageTitleDesempenho';
 
 export const Desempenho = () => {
   return (
     <>
+      <BoxPageTitleDesempenho></BoxPageTitleDesempenho>
       <OrganismeTabPanel arrayOpcoes={[
         {
           nomeTab: 'Colaborador',
