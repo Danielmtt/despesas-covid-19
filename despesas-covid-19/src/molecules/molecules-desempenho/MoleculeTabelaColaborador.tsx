e25a10ae88c1173cf7cbf094def427a484ab3c0a
marcela.fonseca <marcelafonseca@hotmail.com.br>
2021-03-01 19:45:41 -0300

    26-criar-ação-de-delatar-na-linha-no-grid-colaborador - adicionada chamada ao endpoint de deletar colaborador

--- despesas-covid-19/src/molecules/molecules-desempenho/MoleculeTabelaColaborador.tsx
+++ despesas-covid-19/src/molecules/molecules-desempenho/MoleculeTabelaColaborador.tsx
@@ -1,39 +1,15 @@
 import React, { useContext, useEffect, useState } from 'react';
-import { DataGrid, RowsProp, ColDef } from '@material-ui/data-grid';
-import { getColaboradores } from '../../services/Service-desempenho';
-import { DesempenhoAvaliacoesContext } from '../../providers/desempenho-avaliacoes-context';
-import React, { useEffect, useState } from 'react';
 import { DataGrid, RowsProp, ColDef, CellParams } from '@material-ui/data-grid';
 import { deleteColaborador, getColaboradores } from '../../services/Service-desempenho';
 import { AtomSweetAlert } from '../../atoms/atoms-desempenho/atom-sweet-alert';
 import { AtomGridBotaoDeletar } from '../../atoms/atoms-desempenho/atom-grid-botao-deletar';
 import { desejaDeletarColaborador } from '../../settings/consts/MessagesSweetAlert';
+import { notifySuccess, notifyError } from '../../services/ServiceApi';
+import { DesempenhoAvaliacoesContext } from '../../providers/desempenho-avaliacoes-context';
 
 // let rows: RowsProp = [
 //   { id: 1, nome: 'Nome do Colaborador', sigla: 'NDC' },
 // ];
-function handleDeleteColaborador(colaborador: any) {
-  deleteColaborador(colaborador.id)
-    .then((response) => {
-      if (response.status === 204) {
-        notifySuccess(`O colaborador ${colaborador.nome} foi deletado com sucesso`);
-      } else if (response.status === 400) {
-        response.json().then((json: any) => {
-          toast.error(json.mensagem, {
-            position: 'top-right',
-            autoClose: 5000,
-            closeOnClick: true,
-            pauseOnHover: true,
-          });
-        });
-      }
-    })
-}
-
-function handleActionDeleteAvaliacao(parametros: CellParams) {
-  const avalicaoRow: any = parametros.row;
-  AtomSweetAlert(desejaDeletarColaborador(avalicaoRow.colaborador), () => handleDeleteAvalicao(avalicaoRow));
-}
 
 const columns: ColDef[] = [
   { field: 'nome', headerName: 'Nome', width: 500 },
@@ -70,4 +46,25 @@ export default function App() {
     return (<></>);
   }
 
+  function handleDeleteColaborador(colaborador: any) {
+    deleteColaborador(colaborador.id)
+      .then((response) => {
+        if (response.status === 204) {
+          notifySuccess(`O colaborador ${colaborador.nome} foi deletado com sucesso`);
+          getColaboradores().then((response) => {
+            setRows(response);
+          });
+        } else if (response.status === 400) {
+          response.json().then((json: any) => {
+            notifyError(json.mensagem);
+          });
+        }
+      })
+  }
+
+  function handleActionDeleteAvaliacao(parametros: CellParams) {
+    const colaboradorRow: any = parametros.row;
+    
+    AtomSweetAlert(desejaDeletarColaborador(colaboradorRow.nome), () => handleDeleteColaborador(colaboradorRow));
+  }
 }
